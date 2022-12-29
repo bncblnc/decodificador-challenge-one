adb1b847fc71e2719fbc4fc237bb584a4730a963
bncblnc <bncblnc@gmail.com>
2022-12-29 14:32:39 -0300

    removed repeated code

--- script.js
+++ script.js
@@ -15,54 +15,13 @@ btnPaste.addEventListener("click", paste);
 input.addEventListener("keypress", clearPaste);
 
 function encrypt() {
-  if (!checkInvalid()) return;
-  clearPaste();
-  const text = input.value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-
-  let result = text.replace(/(a)|(e)|(i)|(o)|(u)/g, (char) => {
-    switch (char) {
-      case "a":
-        return "ai";
-      case "e":
-        return "enter";
-      case "i":
-        return "imes";
-      case "o":
-        return "ober";
-      case "u":
-        return "ufat";
-    }
-  });
-
-  displayResult(result);
+  let result = changeText("encrypt");
+  if (result != undefined) displayResult(result);
 }
 
 function decrypt() {
-  if (!checkInvalid()) return;
-  clearPaste();
-  const text = input.value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-
-  let result = text.replace(/(ai)|(enter)|(imes)|(ober)|(ufat)/g, (char) => {
-    switch (char) {
-      case "ai":
-        return "a";
-      case "enter":
-        return "e";
-      case "imes":
-        return "i";
-      case "ober":
-        return "o";
-      case "ufat":
-        return "u";
-    }
-  });
-  displayResult(result);
+  let result = changeText("decrypt");
+  if (result != undefined) displayResult(result);
 }
 
 function copy() {
@@ -73,25 +32,59 @@ function copy() {
   text.setSelectionRange(0, 99999);
   document.execCommand("copy");
 
-  btnCopy.classList.remove("btn-secondary");
-  btnCopy.classList.add("copy");
-  btnCopy.textContent = "✔ Copiado";
-
+  changeCopy("copied");
   btnPaste.classList.remove("hidden");
 }
 
 function paste() {
   input.value = output.value;
-
-  resultOff.classList.toggle("hidden");
-  resultOn.classList.toggle("hidden");
+  toggleResult();
   btnPaste.classList.add("hidden");
-
-  resetCopy();
+  changeCopy("copy");
 }
 
 function clearPaste() {
   if (!btnPaste.classList.contains("hidden")) btnPaste.classList.add("hidden");
+}
+
+function changeText(method) {
+  if (!checkInvalid()) return;
+  clearPaste();
+  const text = input.value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  let result;
+  let originalChars = ["a", "e", "i", "o", "u"];
+  let changedChars = ["ai", "enter", "imes", "ober", "ufat"];
+
+  if (method == "encrypt")
+    result = text.replace(/(a)|(e)|(i)|(o)|(u)/g, (char) =>
+      switchChar(char, originalChars, changedChars)
+    );
+  if (method == "decrypt")
+    result = text.replace(/(ai)|(enter)|(imes)|(ober)|(ufat)/g, (char) =>
+      switchChar(char, changedChars, originalChars)
+    );
+  return result;
+}
+
+function displayResult(result) {
+  output.textContent = result;
+  if (btnCopy.classList.contains("copy")) changeCopy("copy");
+  if (!resultOff.classList.contains("hidden")) toggleResult();
+}
+
+function changeCopy(condition) {
+  if (condition == "copied")
+    copyBtnUpdate("btn-secondary", "copy", "✔ Copiado");
+  if (condition == "copy") copyBtnUpdate("copy", "btn-secondary", "Copiar");
+}
+
+function toggleResult() {
+  resultOff.classList.toggle("hidden");
+  resultOn.classList.toggle("hidden");
 }
 
 function checkInvalid() {
@@ -112,19 +105,23 @@ function isEmpty(text) {
   return text.replace(/\s/g, "");
 }
 
-function displayResult(result) {
-  output.textContent = result;
-
-  if (btnCopy.classList.contains("copy")) resetCopy();
-
-  if (!resultOff.classList.contains("hidden")) {
-    resultOff.classList.toggle("hidden");
-    resultOn.classList.toggle("hidden");
+function switchChar(char, defaultText, newText) {
+  switch (char) {
+    case defaultText[0]:
+      return newText[0];
+    case defaultText[1]:
+      return newText[1];
+    case defaultText[2]:
+      return newText[2];
+    case defaultText[3]:
+      return newText[3];
+    case defaultText[4]:
+      return newText[4];
   }
 }
 
-function resetCopy() {
-  btnCopy.classList.remove("copy");
-  btnCopy.classList.add("btn-secondary");
-  btnCopy.textContent = "Copiar";
+function copyBtnUpdate(classRemove, classAdd, text) {
+  btnCopy.classList.remove(classRemove);
+  btnCopy.classList.add(classAdd);
+  btnCopy.textContent = text;
 }
